89336c4d70f7aec0e3b5e07a8b766d033f46d75d
Abdulfortech <ibnmudi@gmail.com>
2023-08-20 23:23:45 +0100

    other files

---
+++ index.php
@@ -0,0 +1,164 @@
+<!--
+=========================================================
+* Argon Design System - v1.2.2
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-design-system
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+  <title>
+    BAMS | Your best Management Tool
+  </title>
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+  <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+  <!-- CSS Files -->
+  <link rel="stylesheet" href="assets/vendor/nucleo/css/nucleo.css" type="text/css">
+  <link rel="stylesheet" href="assets/vendor/@fortawesome/fontawesome-free/css/all.min.css" type="text/css">
+  <!-- Argon CSS -->
+  <!-- <link rel="stylesheet" href="assets/css/argon.css?v=1.2.0" type="text/css"> -->
+  <link href="assets/css/argon-design-system.css?v=1.2.2" rel="stylesheet" />
+</head>
+
+<body class="login-page">
+  <!-- Navbar -->
+  <?php include 'components/navbar-main.php';?>
+  <!-- End Navbar -->
+  <div class="wrapper">
+    <div class="section section-hero section-shaped">
+      <div class="shape shape-style-1 shape-primary">
+        <span class="span-150"></span>
+        <span class="span-50"></span>
+        <span class="span-50"></span>
+        <span class="span-75"></span>
+        <span class="span-100"></span>
+        <span class="span-75"></span>
+        <span class="span-50"></span>
+        <span class="span-100"></span>
+        <span class="span-50"></span>
+        <span class="span-100"></span>
+      </div>
+      <div class="page-header">
+        <div class="container shape-container d-flex align-items-center py-lg">
+          <div class="col px-0">
+            <div class="row align-items-center justify-content-center">
+              <div class="col-lg-6 text-center">
+                <!-- <img src="./assets/img/brand/white.png" style="width: 200px;" class="img-fluid"> -->
+                <h1 class="text-white font-weight-bolder">BAMS</h1>
+                <p class="lead text-white">Your best management solution that ensure seemless business operation, record keeping and automation.</p>
+                <div class="btn-wrapper mt-5">
+                  <a href="./auth/login" class="btn btn-md text-primary font-weight-bold  btn-white btn-icon mb-3 mb-sm-0">
+                    <span class="btn-inner--icon"><i class="fa fa-user"></i></span>
+                    <span class="btn-inner--text">Sign In</span>
+                  </a>
+                  <a href="pricing" class="btn btn-white text-primary font-weight-bold btn-md mb-3 mb-sm-0" target="_blank">
+                    <span class="btn-inner--icon"><i class="fa fa-money-bill"></i></span>
+                    <span class="btn-inner--text">Pricing</span>
+                  </a>
+                </div>
+                <div class="mt-5">
+                  <small class="font-weight-bold mb-0 mr-2 text-white">*proudly coded by SGR</small>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="separator separator-bottom separator-skew zindex-100">
+        <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
+          <polygon class="fill-white" points="2560 0 2560 100 0 100"></polygon>
+        </svg>
+      </div>
+    </div>
+
+    <div class="section features-1">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 mx-auto text-center">
+            <span class="badge badge-primary badge-pill mb-3">About</span>
+            <h3 class="display-3">What is BAMS?</h3>
+            <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas pariatur soluta, nostrum hic perspiciatis voluptate voluptates vitae quidem, velit rem nulla modi praesentium magni. Deleniti asperiores beatae natus praesentium veniam?</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4">
+            <div class="info">
+              <div class="icon icon-lg icon-shape icon-shape-primary shadow rounded-circle">
+                <i class="ni ni-settings-gear-65"></i>
+              </div>
+              <h6 class="info-title text-uppercase text-primary">Social Conversations</h6>
+              <p class="description opacity-8">We get insulted by others, lose trust for those others. We get back stabbed by friends. It becomes harder for us to give others a hand.</p>
+              <a href="javascript:;" class="text-primary">More about us
+                <i class="ni ni-bold-right text-primary"></i>
+              </a>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="info">
+              <div class="icon icon-lg icon-shape icon-shape-success shadow rounded-circle">
+                <i class="ni ni-atom"></i>
+              </div>
+              <h6 class="info-title text-uppercase text-success">Analyze Performance</h6>
+              <p class="description opacity-8">Don't get your heart broken by people we love, even that we give them all we have. Then we lose family over time. As we live, our hearts turn colder.</p>
+              <a href="javascript:;" class="text-primary">Learn about our products
+                <i class="ni ni-bold-right text-primary"></i>
+              </a>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="info">
+              <div class="icon icon-lg icon-shape icon-shape-warning shadow rounded-circle">
+                <i class="ni ni-world"></i>
+              </div>
+              <h6 class="info-title text-uppercase text-warning">Measure Conversions</h6>
+              <p class="description opacity-8">What else could rust the heart more over time? Blackgold. The time is now for it to be okay to be great. or being a bright color. For standing out.</p>
+              <a href="javascript:;" class="text-primary">Check our documentation
+                <i class="ni ni-bold-right text-primary"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+  <!--   Core JS Files   -->
+  <script src="../assets/js/core/jquery.min.js" type="text/javascript"></script>
+  <script src="../assets/js/core/popper.min.js" type="text/javascript"></script>
+  <script src="../assets/js/core/bootstrap.min.js" type="text/javascript"></script>
+  <script src="../assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+  <!--  Plugin for Switches, full documentation here: http://www.jque.re/plugins/version3/bootstrap.switch/ -->
+  <script src="../assets/js/plugins/bootstrap-switch.js"></script>
+  <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+  <script src="../assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
+  <script src="../assets/js/plugins/moment.min.js"></script>
+  <script src="../assets/js/plugins/datetimepicker.js" type="text/javascript"></script>
+  <script src="../assets/js/plugins/bootstrap-datepicker.min.js"></script>
+  <!-- Control Center for Argon UI Kit: parallax effects, scripts for the example pages etc -->
+  <!--  Google Maps Plugin    -->
+  <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+  <script src="../assets/js/argon-design-system.min.js?v=1.2.2" type="text/javascript"></script>
+  <script src="https://cdn.trackjs.com/agent/v3/latest/t.js"></script>
+  <script>
+    window.TrackJS &&
+      TrackJS.install({
+        token: "ee6fab19c5a04ac1a32a645abde4613a",
+        application: "argon-design-system-pro"
+      });
+  </script>
+</body>
+
+</html>
